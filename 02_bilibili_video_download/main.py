c7f6a1a4cdbb98da5cc69fa6203e559e03f8cf96
ityme <ityme@outlook.com>
2026-01-28 21:19:24 +0800

    demo.

--- 02_bilibili_video_download/main.py
+++ 02_bilibili_video_download/main.py
@@ -2,6 +2,10 @@ import json
 import requests
 import re
 
+
+page_info_path = "./tmp/page_info.json"
+
+
 url = "https://www.bilibili.com/video/BV1rRGqzWEfR/?spm_id_from=333.337.search-card.all.click&vd_source=393172d5dc638338abf4db3a3cf9c4b7"
 
 # 页面搜索: f12 网络 json中:  mp4 video audio play 等关键词用于定位
@@ -15,39 +19,45 @@ headers = {
 def info2local():
     response = requests.get(url, headers=headers)
 
+    info_d = {}
+
     match = re.search(r"<title>(.*?)_哔哩哔哩_bilibili</title>", response.text)
     title = match.group(1) if match else "custom#!+#-未知标题"
 
     match = re.search(
         r"<script>window.__playinfo__=(\{.*?\})</script>", response.text, re.S
     )
-    playinfo_json = json.loads(match.group(1)) if match else None
+    playinfo = json.loads(match.group(1)) if match else None
 
-    with open(
-        "./02_bilibili_video_download/response.text.html", "w", encoding="utf-8"
-    ) as f:
-        f.write(response.text)
+    info_d["title"] = title
+    info_d["playinfo"] = playinfo
+    info_d["response_text"] = response.text
 
-    with open("./02_bilibili_video_download/playinfo.json", "w", encoding="utf-8") as f:
-        f.write(json.dumps(playinfo_json, ensure_ascii=False, indent=2))
+    with open(page_info_path, "w", encoding="utf-8") as f:
+        json.dump(info_d, f, ensure_ascii=False, indent=2)
 
 
 def local2info():
-    with open("./02_bilibili_video_download/playinfo.json", "r", encoding="utf-8") as f:
+    with open(page_info_path, "r", encoding="utf-8") as f:
         playinfo = json.load(f)
 
     return playinfo
 
 
-def parser(playinfo):
-    video_url = playinfo["data"]["dash"]["video"][0]["baseUrl"]
-    audio_url = playinfo["data"]["dash"]["audio"][0]["baseUrl"]
+def parser(page_info):
+    title = page_info["title"]
+    video_url = page_info["playinfo"]["data"]["dash"]["video"][0]["baseUrl"]
+    audio_url = page_info["playinfo"]["data"]["dash"]["audio"][0]["baseUrl"]
 
-    return video_url, audio_url
+    return title, video_url, audio_url
+
+
+def main():
+    info2local()
+    page_info = local2info()
+    title, video_url, audio_url = parser(page_info)
+    print(f"{title=}, {video_url=}, {audio_url=}")
 
 
 if __name__ == "__main__":
-    playinfo = local2info()
-    video_url, audio_url = parser(playinfo)
-    print("video_url:", video_url)
-    print("audio_url:", audio_url)
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # spider.learning
+
+会有很多01_xxx 02_bbb 之类的文件夹.  
+当代码运行时. 需要进入对应01_xxx 目录 
+然后运行 ur main.py (uv run main.py)
